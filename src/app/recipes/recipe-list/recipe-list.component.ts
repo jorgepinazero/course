ecbc8d2cc1c01a6fad6424dc323d85a89444e398
jonndiaz <jonathan.diaz@polimentes.com>
2018-10-22 18:17:35 -0500

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe [] = [
+    new Recipe('Ramen', 'This is how you make ramen', 'https://40aprons.com/wp-content/uploads/2016/01/bacon-ramen-6-920x613.jpg'),
+    new Recipe('Ramen 2', 'Learn how to fucking make ramen', 'https://40aprons.com/wp-content/uploads/2016/01/bacon-ramen-6-920x613.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
